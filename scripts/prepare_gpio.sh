237d04c046c6c08a447231b0f968c5f8487c723a
Andrea Merello <andrea.merello@gmail.com>
2016-03-06 15:58:33 +0100

    add script for initial gpio config (in/out, vcc/vpp off..)

---
+++ scripts/prepare_gpio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+nVPP=16
+nVCC=21
+DO=20
+DI=19
+CK=26
+GPIODIR=/sys/class/gpio
+
+echo $nVPP | sudo tee $GPIODIR/export > /dev/null
+echo $nVCC | sudo tee $GPIODIR/export > /dev/null
+echo $DO | sudo tee $GPIODIR/export > /dev/null
+echo $CK | sudo tee $GPIODIR/export > /dev/null
+echo $DI | sudo tee $GPIODIR/export > /dev/null
+
+echo in | sudo tee $GPIODIR/gpio$DI/direction > /dev/null
+
+echo high | sudo tee $GPIODIR/gpio$nVPP/direction > /dev/null
+echo high | sudo tee $GPIODIR/gpio$nVCC/direction > /dev/null
+
+echo low | sudo tee $GPIODIR/gpio$DO/direction > /dev/null
+echo low | sudo tee $GPIODIR/gpio$CK/direction > /dev/null
